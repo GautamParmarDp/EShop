66c91fe03b8398be60af5f3f09f913c4dac6627b
GautamParmarDp <gautamparmardp@gmail.com>
2024-07-21 13:46:44 +0530

    created Product List Reducer & Action(has not fired it for the use yet, just created)

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,8 +2,11 @@ import {combineReducers,applyMiddleware} from 'redux'
 import { configureStore} from '@reduxjs/toolkit'
 import {thunk} from 'redux-thunk'
 import {composeWithDevTools} from '@redux-devtools/extension'  
+import {productListReducers} from './reducers/productReducers'
 
-const reducers = combineReducers({})
+const reducers = combineReducers({
+    productList:productListReducers,
+})
 const initialState = {}
 const middleware = [thunk]
 
