f4525790c6c5c14b32a04e187c79bdc77c025b5c
GautamParmarDp <gautamparmardp@gmail.com>
2024-07-24 16:33:13 +0530

    customise user(that we get along token) by serializer method fields and return custom attributes rather than coustomising complete user Model

--- backend/base/serializer.py
+++ backend/base/serializer.py
@@ -8,6 +8,27 @@ class ProductSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class UserSerializer(serializers.ModelSerializer):
+    name= serializers.SerializerMethodField(read_only=True)
+    
+    _id= serializers.SerializerMethodField(read_only=True) 
+    #bcz in frontend api we used _id so rather than changing complete model just changing it here in response
+    isAdmin= serializers.SerializerMethodField(read_only=True) 
+
     class Meta:
         model = User
-        fields = ['id','username','email']
+        fields = ['id','_id','username','email','name','isAdmin']
+    
+    def get_name(self,obj):
+        # obj is User Object here
+        name = obj.first_name
+        if name == '':
+            name = obj.email
+        return name
+    
+    def get__id(self,obj):
+        _id=obj.id
+        return _id
+    
+    def get_isAdmin(self,obj):
+        return obj.is_staff
+    
