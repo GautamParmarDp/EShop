e184841a93fc0f35d6eb14b31dc1f0a0bb6cea1c
GautamParmarDp <gautamparmardp@gmail.com>
2024-08-07 23:39:11 +0530

    create Product Reviews Frontend => frontend(ProductScreen Component,Constant, Action,Reducer & Store)

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -20,6 +20,10 @@ import {
     PRODUCT_UPDATE_REQUEST,
     PRODUCT_UPDATE_SUCCESS,
     PRODUCT_UPDATE_FAIL,
+
+    PRODUCT_CREATE_REVIEW_REQUEST,
+    PRODUCT_CREATE_REVIEW_SUCCESS,
+    PRODUCT_CREATE_REVIEW_FAIL,
 } from '../constants/productConstants'
 
 export const listProducts = ()=> async (dispatch) => {
@@ -128,6 +132,30 @@ export const updateProduct = (product) => async(dispatch,getState) => {
     catch(error){
         dispatch( { 
             type:PRODUCT_UPDATE_FAIL , 
+            payload:error.response && error.response.data.detail 
+            ?error.response.data.detail
+            :error.message,
+        })
+    }
+}
+
+export const createProductReview = (productId,review) => async(dispatch,getState) => {
+    try{
+        dispatch({ type:PRODUCT_CREATE_REVIEW_REQUEST })
+
+        const{ userLogin:{userInfo} } = getState() //user should be authenticated(loggedIn) to post a product review from the path /api/products/${productId}/reviews/ so we are fetching token of loggedIn user and sending it in headers
+        const config = {
+            headers:{ 
+                'Content-type':'application/json' ,
+                Authorization :`Bearer ${userInfo.token}`
+            }
+        }
+        const {data} = await axios.post(`/api/products/${productId}/reviews/` , review ,config )
+        dispatch({ type:PRODUCT_CREATE_REVIEW_SUCCESS , payload:data }) 
+    }
+    catch(error){
+        dispatch( { 
+            type:PRODUCT_CREATE_REVIEW_FAIL , 
             payload:error.response && error.response.data.detail 
             ?error.response.data.detail
             :error.message,
--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -21,7 +21,12 @@ import {
     PRODUCT_CREATE_SUCCESS,
     PRODUCT_CREATE_FAIL,
     PRODUCT_CREATE_RESET,
-    } from '../constants/productConstants'
+
+    PRODUCT_CREATE_REVIEW_REQUEST,
+    PRODUCT_CREATE_REVIEW_SUCCESS,
+    PRODUCT_CREATE_REVIEW_FAIL,
+    PRODUCT_CREATE_REVIEW_RESET,
+} from '../constants/productConstants'
 
 export const productListReducers = (state ={products:[]},action)=>{
 
@@ -93,6 +98,23 @@ export const productUpdateReducers = (state ={product:{}},action)=>{
             return{ loading:false , error:action.payload} 
         case PRODUCT_UPDATE_RESET:
                 return{ product:{} }
+    
+        default:
+            return state
+        }
+}
+
+export const productReviewCreateReducers = (state ={ },action)=>{
+
+    switch(action.type){
+        case PRODUCT_CREATE_REVIEW_REQUEST:
+            return{ loading:true }
+        case PRODUCT_CREATE_REVIEW_SUCCESS:
+            return{ loading:false , success:true }
+        case PRODUCT_CREATE_REVIEW_FAIL:
+            return{ loading:false , error:action.payload} 
+        case PRODUCT_CREATE_REVIEW_RESET:
+                return{}
     
         default:
             return state
